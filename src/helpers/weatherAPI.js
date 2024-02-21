b55ff9425aae07af644368d9528d14e496a7c42f
Claudio Meira <claudiomeirabs@gmail.com>
2024-02-21 18:51:09 -0300

    Requisito um feito e testado

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,9 +1,22 @@
 // Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
+  const data = await response.json();
+  if (!response.ok || !data.length) {
+    window.alert('Nenhuma cidade encontrada');
+    return [];
+  }
+  return data;
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
 //   seu código aqui
 };
+// Swal.fire({
+  // icon: "error",
+  // title: "Oops...",
+  // text: "Something went wrong!",
+  // footer: '<a href="#">Why do I have this issue?</a>'
+// });
